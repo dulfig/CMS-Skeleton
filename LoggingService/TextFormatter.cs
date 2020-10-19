395d1d29f0ccf6409e45f9403b48ec1f1d5a3247
dulfig <donulfig@gmail.com>
2020-10-19 05:16:57 -0500

    Added logging service using Serilog as well as basic configuration for the log files

---
+++ LoggingService/TextFormatter.cs
@@ -0,0 +1,38 @@
+﻿using Serilog.Events;
+using Serilog.Formatting;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace LoggingService
+{
+    public class TextFormatter : ITextFormatter
+    {
+        public void Format(LogEvent logEvent, TextWriter output)
+        {
+            //Just want to show Error/Debug/Warning
+            if (logEvent.Level.ToString() != "Information")
+            {
+                output.WriteLine("----------------------------------------------------------------");
+                output.WriteLine($"TimeStamp : {logEvent.Timestamp} | Level : {logEvent.Level} |");
+                output.WriteLine("----------------------------------------------------------------");
+                foreach (var item in logEvent.Properties)
+                {
+                    output.WriteLine(item.Key + " : " + item.Value);
+                }
+                //If there is an exception print the details
+                if(logEvent.Exception != null)
+                {
+                    output.WriteLine("-----------------------EXCEPTION DETAILS-----------------------");
+                    output.WriteLine("Exception - {0}", logEvent.Exception);
+                    output.WriteLine("StackTrace - {0}", logEvent.Exception.StackTrace);
+                    output.WriteLine("Message - {0}", logEvent.Exception.Message);
+                    output.WriteLine("Source - {0}", logEvent.Exception.Source);
+                    output.WriteLine("InnerException - {0}", logEvent.Exception.InnerException);
+                }
+                output.WriteLine("---------------------------------------------------------------------");
+            }
+        }
+    }
+}
