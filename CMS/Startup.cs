94fb9c65203c5918083127dc2934fcfa3ef14fbe
dulfig <donulfig@gmail.com>
2020-10-23 14:57:54 -0500

    Setup for admin homepage using razor that is able to be changed during runtime

--- CMS/Startup.cs
+++ CMS/Startup.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.SpaServices.AngularCli;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
@@ -64,6 +65,8 @@ namespace CMS
                 options.User.RequireUniqueEmail = identityDefaultOp.UserReqUniqueEmail;
                 options.SignIn.RequireConfirmedEmail = identityDefaultOp.SignInReqConfirmEmail;
             }).AddEntityFrameworkStores<AppDbContext>().AddDefaultTokenProviders();
+            //Allows html changes in using Razor(cshtml) in real time
+            services.AddMvc().AddControllersAsServices().AddRazorRuntimeCompilation().SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -91,6 +94,9 @@ namespace CMS
 
             app.UseEndpoints(endpoints =>
             {
+                endpoints.MapControllerRoute(
+                    name: "areas",
+                    pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller}/{action=Index}/{id?}");
