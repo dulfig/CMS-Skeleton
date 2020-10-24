d53bdf2e91b763ff79f3e4ba8a0da57ee279f643
dulfig <donulfig@gmail.com>
2020-10-24 03:17:19 -0500

    Created JQuery function to handle sideBar collapsing and expanding

---
+++ CMS/wwwroot/lib/custom/js/admin/admin.js
@@ -0,0 +1,11 @@
+﻿(function () {
+    'use strict';
+    $(initSideBar);
+    function initSideBar() {
+        $(document).ready(function () {
+            $('#sidebarCollapse').on('click', function () {
+                $('#sidebar').toggleClass('active');
+            });
+        });
+    }
+})();
